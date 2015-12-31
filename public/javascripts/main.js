77be7b69815c8e7332ec26b7b4a6c98414358f82
Talon Daniels <talondaniels@gmail.com>
2015-12-31 14:24:26 -0700

    Revert incorrect configuration

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    io.set('browser client gzip', true);
-    io.set('browser client minification', true);
-    io.set('browser client etag', true);
     var socket = io.connect(window.location.origin);
     var alias = window.location.pathname.slice(3);
     socket.on('connect', function() {
