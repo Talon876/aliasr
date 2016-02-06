e033e02f24233c9472942c7f26c1bc79d004dec5
Talon Daniels <talondaniels@gmail.com>
2016-02-06 16:28:34 -0700

    Improve front-end layout

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 var aliasdb = require('../aliasdb');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     aliasdb.size(function (err, aliasCount) {
         if (err) {
             res.render('index', {title: 'Aliasr', hasCount: false});
         } else {
             res.render('index', {title: 'Aliasr', aliasCount: aliasCount, hasCount: true});
+        }
+    });
+});
+
+router.get('/about', function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/stats', function(req, res) {
+    aliasdb.size(function (err, aliasCount) {
+        if (err) {
+            res.render('stats', {hasCount: false});
+        } else {
+            res.render('stats', {aliasCount: aliasCount, hasCount: true});
         }
     });
 });
